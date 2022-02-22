c136a0a8f1cf6dabb3d83ff787d8d6f676a85de5
0str1ch <hi@jeremysmith.dev>
2022-02-22 12:54:58 -0500

    update config and manifest

--- config.lua
+++ config.lua
@@ -1,18 +1,19 @@
 Config = {}
 
 Config.Jobs = {
-    ['police'] = vector3(448.4, -973.2, 30.6),
-    ['ambulance'] = vector3(310.1, -599.43, 43.29),
+    ['police'] = vector3(460.12, -985.4, 30.73),
+    ['ambulance'] = vector3(334.88, -592.56, 43.28),
     ['realestate'] = vector3(-124.786, -641.486, 168.820),
     ['taxi'] = vector3(903.32, -170.55, 74.0),
-    ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
+    ['cardealer'] = vector3(-24.48, -1101.95, 27.27),
     ['mechanic'] = vector3(-347.41, -133.48, 39.01)
 }
 
 Config.Gangs = {
     ['lostmc'] = vector3(0, 0, 0),
-    ['ballas'] = vector3(0,0, 0),
+    ['ballas'] = vector3(86.75, -1958.28, 21.12),
     ['vagos'] = vector3(0, 0, 0),
     ['cartel'] = vector3(0, 0, 0),
-    ['families'] = vector3(0, 0, 0)
+    ['families'] = vector3(0, 0, 0),
+    ['triads'] = vector3(0, 0, 0)
 }
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'qb-bossmenu'
+description 'qb-management'
 version '2.0.0'
 
 client_scripts {
