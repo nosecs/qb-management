abcbf8fb05d2df8aa481e7b3b5ff6b4c585e89f1
MrJayGamerTTV <mr.j.gamer98@gmail.com>
2022-03-27 16:43:30 +0300

    Added pizzeria-bossmenu

--- config.lua
+++ config.lua
@@ -6,7 +6,8 @@ Config.Jobs = {
     ['realestate'] = vector3(-124.786, -641.486, 168.820),
     ['taxi'] = vector3(903.32, -170.55, 74.0),
     ['cardealer'] = vector3(-24.48, -1101.95, 27.27),
-    ['mechanic'] = vector3(-347.41, -133.48, 39.01)
+    ['mechanic'] = vector3(-347.41, -133.48, 39.01),
+    ['pizza'] = vector3(795.48, -749.51, 31.27),
 }
 
 Config.Gangs = {
